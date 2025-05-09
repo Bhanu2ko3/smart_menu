dee7272f7c95d4529a3c3ae508939fe2ab606d7d
Bhanu2k03 <bhanukabandara039@gmail.com>
2025-05-09 10:19:16 +0530

    uu

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
     darkMode: 'class', // 👈 Add this line for class-based dark mode
     content: [
-      "./src/**/*.{js,jsx,ts,tsx}", // 👈 src folder තුළ තියෙන හැම file එකක්ම Tailwind වලින් අරගන්න
+      "./src/**/*.{js,jsx,ts,tsx}", // 
     ],
     theme: {
       extend: {},
